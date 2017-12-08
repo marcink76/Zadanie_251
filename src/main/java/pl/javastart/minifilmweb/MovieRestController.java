d8ab7e1f60d534c2acc3269002c0f5948853d2e6
Marcin Konieczny <marcin_k@coni.pl>
2017-12-08 13:30:17 +0100

    Działa

--- src/main/java/pl/javastart/minifilmweb/MovieRestController.java
+++ src/main/java/pl/javastart/minifilmweb/MovieRestController.java
@@ -1,21 +1,53 @@
 package pl.javastart.minifilmweb;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//        GET /api/movies/
+//        GET /api/movies/{id}
+//        POST /api/movies/
+//        PUT /api/movies/{id}
+//        DELETE /api/movies/{id}
 @RestController
+@RequestMapping("/api/movies")
 public class MovieRestController {
 
     @Autowired
     private MovieRepository movieRepository;
 
-    @GetMapping("/api/movies")
+    @GetMapping("")
     public List<Movie> getAllMovies() {
         System.out.println("Odpytanie o wszystkie filmy");
         return movieRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Movie getMovie(@PathVariable("id") long id) {
+        return movieRepository.findOne(id);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> addMovie(@RequestBody Movie movie) {
+        Movie movie1 = movieRepository.save(movie);
+        return ResponseEntity.ok(movie1);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable("id") long id) {
+        movieRepository.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public void editMovie(@RequestBody Movie movie,
+                          @PathVariable("id") long id) {
+        Movie movieEdit = movieRepository.findOne(id);
+        //movieEdit.setId(movie.getId());
+        movieEdit.setTitle(movie.getTitle());
+        movieEdit.setPremiereDate(movie.getPremiereDate());
+        movieRepository.save(movieEdit);
+    }
 }
